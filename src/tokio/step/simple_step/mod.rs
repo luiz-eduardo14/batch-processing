a98cc6790cddb7cfb7b3f542518e4ad1bd19542f
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-04-01 02:52:37 -0300

    docs: add documentation for async simple step

--- src/tokio/step/simple_step/mod.rs
+++ src/tokio/step/simple_step/mod.rs
@@ -1,15 +1,35 @@
 use crate::tokio::step::{AsyncStep, DeciderCallback, DynAsyncCallback};
 use crate::tokio::step::step_builder::AsyncStepBuilderTrait;
 
+/// This trait defines methods for building asynchronous steps with simple configurations.
 pub trait AsyncSimpleStepBuilderTrait<I, O> {
+    /// Sets the tasklet for the step.
+    ///
+    /// # Arguments
+    ///
+    /// * `step_callback` - The callback function for the step.
+    ///
+    /// # Returns `Self`
+    ///
+    /// Returns a modified builder instance.
     fn tasklet(self, step_callback: Box<DynAsyncCallback<Result<String, String>>>) -> Self;
 }
 
+/// A builder struct for constructing asynchronous simple steps.
 pub struct AsyncSimpleStepBuilder {
     step: AsyncStep,
 }
 
 impl AsyncStepBuilderTrait for AsyncSimpleStepBuilder {
+    /// Sets the decider callback for the step.
+    ///
+    /// # Arguments
+    ///
+    /// * `decider` - The decider callback function.
+    ///
+    /// # Returns `Self`
+    ///
+    /// Returns a modified builder instance.
     fn decider(self, decider: DeciderCallback) -> Self {
         AsyncSimpleStepBuilder {
             step: AsyncStep {
@@ -19,6 +39,11 @@ impl AsyncStepBuilderTrait for AsyncSimpleStepBuilder {
         }
     }
 
+    /// Sets the step to be tolerant to thrown errors.
+    ///
+    /// # Returns `Self`
+    ///
+    /// Returns a modified builder instance.
     fn throw_tolerant(self) -> Self {
         AsyncSimpleStepBuilder {
             step: AsyncStep {
@@ -28,7 +53,15 @@ impl AsyncStepBuilderTrait for AsyncSimpleStepBuilder {
         }
     }
 
-    #[inline]
+    /// Initializes a new builder instance with the given name.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - The name of the step.
+    ///
+    /// # Returns `Self`
+    ///
+    /// Returns a new builder instance.
     fn get(name: String) -> Self {
         AsyncSimpleStepBuilder {
             step: AsyncStep {
@@ -40,6 +73,15 @@ impl AsyncStepBuilderTrait for AsyncSimpleStepBuilder {
         }
     }
 
+    /// Validates the builder configuration.
+    ///
+    /// # Panics
+    ///
+    /// Panics if tasklet or name is not provided.
+    ///
+    /// # Returns `Self`
+    ///
+    /// Returns a modified builder instance if validation succeeds.
     fn validate(self) -> Self {
         if self.step.callback.is_none() {
             panic!("Tasklet is required");
@@ -52,6 +94,11 @@ impl AsyncStepBuilderTrait for AsyncSimpleStepBuilder {
         return self;
     }
 
+    /// Builds and returns the configured asynchronous step.
+    ///
+    /// # Returns `AsyncStep`
+    ///
+    /// Returns the configured asynchronous step.
     fn build(self) -> AsyncStep {
         let current_self = self.validate();
         return current_self.step;
@@ -59,6 +106,15 @@ impl AsyncStepBuilderTrait for AsyncSimpleStepBuilder {
 }
 
 impl AsyncSimpleStepBuilderTrait<fn(), fn()> for AsyncSimpleStepBuilder {
+    /// Sets the tasklet for the step.
+    ///
+    /// # Arguments
+    ///
+    /// * `step_callback` - The callback function for the step.
+    ///
+    /// # Returns `Self`
+    ///
+    /// Returns a modified builder instance.
     fn tasklet(self, step_callback: Box<DynAsyncCallback<Result<String, String>>>) -> Self {
         return AsyncSimpleStepBuilder {
             step: AsyncStep {
@@ -69,6 +125,15 @@ impl AsyncSimpleStepBuilderTrait<fn(), fn()> for AsyncSimpleStepBuilder {
     }
 }
 
+/// Returns a new `AsyncSimpleStepBuilder` instance with the given name.
+///
+/// # Arguments
+///
+/// * `name` - The name of the step.
+///
+/// # Returns `Self`
+///
+/// Returns a new `AsyncSimpleStepBuilder` instance.
 pub fn get(name: String) -> AsyncSimpleStepBuilder {
     AsyncSimpleStepBuilder::get(name)
 }
